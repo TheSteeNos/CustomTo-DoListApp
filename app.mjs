d885099ea2f141598b17bd7da1dea1ff0cd9d0d4
TheSteeNos <davidsteens2001@gmail.com>
2025-03-30 19:28:18 +0200

    Established a design

--- app.mjs
+++ app.mjs
@@ -1,1 +1,14 @@
 console.log("APP");
+const bodyStyle = document.body.style;
+const title = document.getElementById("listTitle");
+
+//Page and word Colors
+bodyStyle.backgroundColor = "#485464";
+bodyStyle.color = "white";
+bodyStyle.fontFamily = "Century Gothic, sans-serif";
+
+//Title's Font Family, Size and Weight, Position and Color section
+title.style.fontWeight = "bold";
+title.style.fontColor = "#FFFFFF";
+title.style.fontSize = "48px";
+title.style.marginLeft = "50px";
